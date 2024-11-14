f2a74cc653ed44a48713cf28daa7205aeaf96479
whatmyusename <87497082+whatmeusername@users.noreply.github.com>
2024-11-15 00:48:26 +0300

    added svgr, react-router-dom, inited telegram css vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,64 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { LaunchParamsProvider } from './providers/LaunchParamsProvider';
-// import { ApiAxios } from './utils/AxiosInstances';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import { UserProvider } from './providers/UserAuthProvider';
-import { UserTonWalletProvider } from './providers/UserTonWallet';
+
+import { useIntegration } from '@telegram-apps/react-router-integration';
+import { bindThemeParamsCSSVars, initNavigator, initThemeParams } from '@telegram-apps/sdk';
+import { Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+
+function Home() {
+	const [count, setCount] = useState(0);
+	return (
+		<>
+			<div>
+				<a href="https://vite.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+				<Link to={'/profile'}>Profile</Link>
+			</div>
+		</>
+	);
+}
+
+function BottomNavigator() {
+	<div className="bottom-navigator_wrapper"></div>;
+}
+
+function UserProfile() {
+	return <div>Hello</div>;
+}
 
 function App() {
-	const [count, setCount] = useState(0);
+	const navigator = useMemo(() => initNavigator('app-navigation-state'), []);
+	const [location, reactNavigator] = useIntegration(navigator);
+
+	useEffect(() => {
+		navigator.attach();
+		return () => navigator.detach();
+	}, [navigator]);
+
+	const [tp] = initThemeParams();
+	bindThemeParamsCSSVars(tp);
 
 	return (
-		<TonConnectUIProvider manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json">
-			<LaunchParamsProvider>
-				<UserTonWalletProvider>
-					<UserProvider>
-						<div>
-							<a href="https://vite.dev" target="_blank">
-								<img src={viteLogo} className="logo" alt="Vite logo" />
-							</a>
-							<a href="https://react.dev" target="_blank">
-								<img src={reactLogo} className="logo react" alt="React logo" />
-							</a>
-						</div>
-						<h1>Vite + React</h1>
-						<div className="card">
-							<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-							<p>
-								Edit <code>src/App.tsx</code> and save to test HMR
-							</p>
-						</div>
-					</UserProvider>
-				</UserTonWalletProvider>
-			</LaunchParamsProvider>
-		</TonConnectUIProvider>
+		<Router location={location} navigator={reactNavigator}>
+			<Routes>
+				<Route path={'/'} Component={Home} />
+				<Route path={'/profile'} Component={UserProfile} />
+				<Route path={'*'} element={<Navigate to={'/'} />} />
+			</Routes>
+		</Router>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { LaunchParamsProvider } from './providers/LaunchParamsProvider.tsx';
+import { UserProvider } from './providers/UserAuthProvider.tsx';
+import { UserTonWalletProvider } from './providers/UserTonWallet.tsx';
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+	<TonConnectUIProvider manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json">
+		<LaunchParamsProvider>
+			<UserTonWalletProvider>
+				<UserProvider>
+					<App />
+				</UserProvider>
+			</UserTonWalletProvider>
+		</LaunchParamsProvider>
+	</TonConnectUIProvider>,
 );
