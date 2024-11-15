d78af7395ce124f0645cfd77e46d72848c84df38
whatmyusename <87497082+whatmeusername@users.noreply.github.com>
2024-11-16 01:36:49 +0300

    added prtops to BottomNavigator, added haptic on link click at bottom navigator

--- src/apps/UserProfile/UserProfile.tsx
+++ src/apps/UserProfile/UserProfile.tsx
@@ -2,11 +2,27 @@ import { motion } from 'motion/react';
 import { BottomNavigator } from '../../shared/components/BottomNavigator/BottomNavigator';
 import { ReactElement } from 'react';
 import { Outlet } from 'react-router-dom';
+import { WalletIcon, HomeIcon } from '@shared/icons';
+
+const NavigatorData = [
+	{
+		url: '/profile/wallet',
+		icon: (className: string) => <WalletIcon className={className} />,
+		haptic: 'light' as const,
+		label: 'home',
+	},
+	{
+		url: '/profile/home',
+		icon: (className: string) => <HomeIcon className={className} />,
+		haptic: 'light' as const,
+		label: 'home',
+	},
+];
 
 function UserProfileOutlet(): ReactElement {
 	return (
 		<motion.div exit={{ opacity: 0 }}>
-			<BottomNavigator />
+			<BottomNavigator NavigatorLinksData={NavigatorData} />
 			<Outlet />
 		</motion.div>
 	);
--- src/shared/components/BottomNavigator/BottomNavigator.tsx
+++ src/shared/components/BottomNavigator/BottomNavigator.tsx
@@ -3,31 +3,46 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
 import './BottomNavigator.scss';
-import { HomeIcon } from '../../icons';
 import { BNAnimation } from './animation';
+import { hapticFeedback, ImpactHapticFeedbackStyle } from '@telegram-apps/sdk-react';
 
-const BottomNavigatorLink = () => {
+interface NavigatorData {
+	url: string;
+	icon: (className: string) => ReactElement;
+	haptic: ImpactHapticFeedbackStyle;
+	label: string;
+}
+
+const BottomNavigatorLink = ({ NavigatorLinkData }: { NavigatorLinkData: NavigatorData }) => {
 	return (
-		<Link to={'/'} className="bottom-navigator_link">
-			<HomeIcon className="bottom-navigator_link-icon" />
-			<p className="bottom-navigator_link-label">home</p>
+		<Link
+			to={'/'}
+			className="bottom-navigator_link"
+			onClick={() => {
+				if (hapticFeedback.impactOccurred.isAvailable()) {
+					hapticFeedback.impactOccurred(NavigatorLinkData.haptic);
+				}
+			}}
+		>
+			{NavigatorLinkData.icon('bottom-navigator_link-icon')}
+			<p className="bottom-navigator_link-label">{NavigatorLinkData.label}</p>
 		</Link>
 	);
 };
 
-function BottomNavigator(): ReactElement {
+function BottomNavigator({ NavigatorLinksData }: { NavigatorLinksData: NavigatorData[] }): ReactElement {
 	return (
 		<div className="bottom-navigator_container">
 			<motion.div variants={BNAnimation} initial="initial" animate="animate" className="bottom-navigator_wrapper">
 				<div className="bottom-navigator_links">
-					<BottomNavigatorLink />
-					<BottomNavigatorLink />
-					<BottomNavigatorLink />
-					<BottomNavigatorLink />
+					{NavigatorLinksData.map((NavigatorLinkData) => {
+						return <BottomNavigatorLink NavigatorLinkData={NavigatorLinkData} key={NavigatorLinkData.url} />;
+					})}
 				</div>
 			</motion.div>
 		</div>
 	);
 }
 
+export type { NavigatorData };
 export { BottomNavigator };
